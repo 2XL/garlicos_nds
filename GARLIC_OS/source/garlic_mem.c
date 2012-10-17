8e47afd997db7d02a1084b6362cdae79e912db13
Cristian Soriano <cristian.soriano@estudiants.urv.cat>
2012-10-17 22:35:44 +0200

    ProgM -  Versión final (esperemos...)

--- GARLIC_OS/source/garlic_mem.c
+++ GARLIC_OS/source/garlic_mem.c
@@ -15,6 +15,32 @@
 
 
 
+#define MEMORIA_PROGRAMA 0x02100000
+
+
+
+// Actualiza la posición y devuelve el word
+int juntar_word (char *file, int* pos)
+{
+	int num = *pos;
+	*pos = *pos + 4;
+	return (file[num+3] << 24) | (file[num+2] << 16) | (file[num+1] << 8) | file[num];
+}
+
+
+// Actualiza la posición y devuelve el half-word
+int juntar_half (char *file, int* pos)
+{
+	int num = *pos;
+	*pos = *pos + 2;
+	return (file[num+1] << 8) | file[num];
+}
+
+
+
+
+
+
 /* _gm_relocatar: a partir de una dirección de memoria (destino), de un vector
 			que contiene todo el fichero ELF (file) y de una dirección 'virtual'
 			de inicio de un segmento (pAddr), esta función interpretará los
@@ -24,10 +50,76 @@
 void _gm_relocatar(unsigned int *destino, char *file, unsigned int pAddr)
 {
 	
-	//file[(int)destino] = (int)0x02100000 + file[(int)destino] - pAddr ;
-	*(destino) = (int)0x02100000 + *(destino) - pAddr ;
+	int pos=24;									//pos del entry
+	
+	pos=pos+8;	//Saltamos entry y phoff		//pos 24 -> 32
+	int shoff =		juntar_word (file, &pos);	//pos 32 -> 36
+	pos=pos+10;	//Saltamos phentsize y phnum	//pos 36 -> 46
+	int shentsize =	juntar_half (file, &pos);	//pos 46 -> 48
+	int shnum = 	juntar_half (file, &pos);	//pos 48 -> 50
+	
+	
+	// Variables de las secciones
+	pos = shoff;
+	int pos_ini = pos;
+	int contador, sh_offset, sh_size, sh_link, sh_info, sh_entsize;
+	
+	// Variables de relocalizaciones
+	int r_offset, contador_r, pos_r, pos_r_ini;
+	unsigned int* direccion;					// Para trabajar con la dirección destino sin cambiar su variable
+	
+	
+	
+	for (contador=0; contador<shnum; contador++, pos=pos_ini+shentsize, pos_ini=pos)	// Mientras haya más secciones...
+	{
+		pos=pos+4;		// Nos saltamos sh_name
+		// Sólo nos interesan los que tengan type 9
+		if ( juntar_word (file, &pos) == 9)	// si sh_type es 9 (SHT REL)  pos  shoff -> shoff+4
+		{
+			pos=pos+8;	// Saltamos sh_flags y sh_addr    pos  shoff+4  -> shoff+12
+			sh_offset = juntar_word (file, &pos);		//pos  shoff+12 -> shoff+16
+			sh_size = 	juntar_word (file, &pos);		//pos  shoff+16 -> shoff+20
+			sh_link = 	juntar_word (file, &pos);		//pos  shoff+20 -> shoff+24
+			sh_info = 	juntar_word (file, &pos);		//pos  shoff+24 -> shoff+28
+			pos=pos+4;	// Nos saltamos sh_addralign  	  pos  shoff+28 -> shoff+32
+			sh_entsize =juntar_word (file, &pos);		//pos  shoff+32 -> shoff+36
+			
+			
+			
+			// Relocalizaciones
+			pos_r = sh_offset;
+			pos_r_ini = pos_r;
+			printf(">> seccion relocalizadores ( ):\n");
+			
+			for (contador_r=0; contador_r<(sh_size/sh_entsize); contador_r++, pos_r=pos_r_ini+sh_entsize, pos_r_ini=pos_r)
+			{
+				// if r_info == 514 (0x0202)  - r_info tiene un offset inicial de 4 bytes respecto a r_offset, de ahí que sume para acceder al info y luego reste para el offset
+				pos_r = pos_r+4;
+				if ( juntar_word (file, &pos_r) == 514)		//pos_r  sh_offset+4 -> sh_offset+8
+				{
+					pos_r = pos_r-8;							//pos_r  sh_offset+8 -> sh_offset
+					r_offset =	juntar_word (file, &pos_r);		//pos_r  sh_offset   -> sh_offset+4
+					
+					printf(">>> rel %d (R_ARM_ABS32): 0x%X\n", contador_r, r_offset);
+					
+					direccion = (unsigned int*)destino;
+					direccion = (unsigned int*)((int)direccion + r_offset - pAddr);
+					*(direccion) = (int)destino + *(direccion) - pAddr ;
+					
+				}	//endif r
+				
+			}	// endfor r
+			
+		}	// endif sh
+		
+	}	// endfor sh
+	
 	
 }
+
+
+
+
 
 /* _gm_cargarPrograma: cargar el programa "(keyName).elf" en una zona de memoria
 			libre y relocatar las referencias a los símbolos del programa; */
@@ -66,7 +158,8 @@ intFunc _gm_cargarPrograma(char *keyName) {
 		else
 		{			
 			
-			int entry, phoff, shoff, phentsize, phnum, shentsize, shnum;
+			//int entry, phoff, shoff, phentsize, phnum, shentsize, shnum;
+			int entry, phoff, phentsize, phnum;
 			
 			/*  EI_NIDENT 16 nos dice que e_ident estará en la decimosexta posición, puntero[15]
 				Por lo tanto, si vamos sumando el num de bytes de cada variable de la estructura:
@@ -76,75 +169,48 @@ intFunc _gm_cargarPrograma(char *keyName) {
 				pos[42-43] e_phentsize	pos[44-45] e_phnum		pos[46-47] e_shentsize
 				pos[48-49] e_shnum		pos[50-51] e_shstrnx
 				
-				Como todos los que necesitamos usan más de un byte habrá que concatenarlos.
+				Como los que necesitamos usan más de un byte habrá que unirlos con la función juntar_"tipo"
 			*/
 			
-			entry = 	(puntero[27] << 24) | (puntero[26] << 16) | (puntero[25] << 8) | puntero[24];
-			phoff =		(puntero[31] << 24) | (puntero[30] << 16) | (puntero[29] << 8) | puntero[28];
-			shoff =		(puntero[35] << 24) | (puntero[34] << 16) | (puntero[33] << 8) | puntero[32];
-			phentsize =	(puntero[43] << 8) | puntero[42];
-			phnum = 	(puntero[45] << 8) | puntero[44];
-			shentsize =	(puntero[47] << 8) | puntero[46];
-			shnum = 	(puntero[49] << 8) | puntero[48];
+			int pos=24;									//pos del entry
 			
-			//printf("%d %d %d %d %d %d %d\n", entry, phoff, shoff, phentsize, phnum, shentsize, shnum);
+			entry = 	juntar_word (puntero, &pos);	//pos 24 -> 28
+			phoff =		juntar_word (puntero, &pos);	//pos 28 -> 32
+			pos=pos+10;									//pos 32 -> 42
+			phentsize =	juntar_half (puntero, &pos);	//pos 42 -> 44
+			phnum = 	juntar_half (puntero, &pos);	//pos 44 -> 46
+			
 			
 			printf("> cabeceras de programa :   %d\n", phnum);
 			
 			
-			
-			// Declaración variables de secciones
-			int contador_sh, pos_sh, pos_sh_ini;
-			int sh_offset, sh_size, sh_link, sh_info, sh_entsize;
-			
-			//Declaración variables de relocalizaciones
-			int contador_r, pos_r, pos_r_ini;
-			int r_offset;
-			unsigned int* destino;
-			
 			// Variables segmentos
-			int contador;
+			int destino = MEMORIA_PROGRAMA;		// 0x02100000
 			int mem_offset = 0;					// Para determinar dónde cargar segmentos si hay más de 1
-			int pos = phoff;
+			pos = phoff;
 			int pos_ini = pos;
-			int p_offset, p_paddr, p_filesz, p_memsz, p_flags;
+			int p_paddr = 0;					// Para evitar el warning de que podría no estar declarada
+			int contador, p_offset, p_filesz, p_memsz, p_flags;
 			
 			
 			
 			for (contador=0; contador<phnum; contador++, pos=pos_ini+phentsize, pos_ini=pos)	// Mientras haya más segmentos...
 			{
 				// Sólo nos interesan los que tengan type 1
-				if (( (puntero[pos+3] << 24) | (puntero[pos+2] << 16) | (puntero[pos+1] << 8) | puntero[pos] ) == 1) // Si el p_type es 1 (PT_LOAD)
+				if (juntar_word (puntero, &pos) == 1) // Si el p_type es 1 (PT_LOAD)    	pos phoff -> phoff+4
 				{
-					pos=pos+4;
-					p_offset = 	(puntero[pos+3] << 24) | (puntero[pos+2] << 16) | (puntero[pos+1] << 8) | puntero[pos];
-					pos=pos+8;	// nos saltamos p_vaddr
-					p_paddr = 	(puntero[pos+3] << 24) | (puntero[pos+2] << 16) | (puntero[pos+1] << 8) | puntero[pos];
-					pos=pos+4;
-					p_filesz = 	(puntero[pos+3] << 24) | (puntero[pos+2] << 16) | (puntero[pos+1] << 8) | puntero[pos];
-					pos=pos+4;
-					p_memsz = 	(puntero[pos+3] << 24) | (puntero[pos+2] << 16) | (puntero[pos+1] << 8) | puntero[pos];
-					pos=pos+4;
-					p_flags = 	(puntero[pos+3] << 24) | (puntero[pos+2] << 16) | (puntero[pos+1] << 8) | puntero[pos];
-					//pos=pos+4;	// nos saltamos p_align
-					
-					//printf("%d %d %d %d %d\n", p_offset, p_paddr, p_filesz, p_memsz, p_flags);
+					p_offset = 	juntar_word (puntero, &pos);	//pos phoff+4  -> phoff+8
+					pos=pos+4;	// nos saltamos p_vaddr			  pos phoff+8  -> phoff+12
+					p_paddr = 	juntar_word (puntero, &pos);	//pos phoff+12 -> phoff+16
+					p_filesz = 	juntar_word (puntero, &pos);	//pos phoff+16 -> phoff+20
+					p_memsz = 	juntar_word (puntero, &pos);	//pos phoff+20 -> phoff+24
+					p_flags = 	juntar_word (puntero, &pos);	//pos phoff+24 -> phoff+28
 					
 					
-					
-					// CARGAR EL CONTENIDO DEL SEGMENTO EN DIRECCIÓN DESTINO
-					// 
-					//dmaCopy((void*)0x02100000, (const void*)puntero[entry], (24*1024));
-					//
-					//DC_FlushRange((const void*)p_offset, p_memsz);
-					
-					
-					//dmaCopy((const void*)p_offset, (void*)0x02100000 + mem_offset, p_memsz);
-					dmaCopy((const void*)&puntero[p_offset], 0x02100000 + mem_offset, p_memsz);
-					mem_offset = mem_offset + p_memsz;
-					//
-					//
-					
+					// Copiar a la memoria
+					destino = destino + mem_offset;							// Se actualiza destino (+0 en la primera iteración)
+					mem_offset = mem_offset + p_memsz;						// Se actualiza el offset de memoria para la siguiente iteración
+					dmaCopy((const void*)&puntero[p_offset], (void*)destino, p_memsz);
 					
 					
 					printf("> segmento num. %d copiado:\n", contador);
@@ -153,60 +219,9 @@ intFunc _gm_cargarPrograma(char *keyName) {
 					printf("   tam. en memoria : %d\n", p_memsz);
 					
 					
-					pos_sh = shoff;										// Trabajaremos con las secciones
-					pos_sh_ini = pos_sh;
+					_gm_relocatar((unsigned int*)destino, puntero, p_paddr);	
 					
-					for (contador_sh=0; contador_sh<shnum; contador_sh++, pos_sh=pos_sh_ini+shentsize, pos_sh_ini=pos_sh)	// Mientras haya más secciones...
-					{
-						pos_sh=pos_sh+4;	// Nos saltamos sh_name
-						// Sólo nos interesan los que tengan type 9
-						if (( (puntero[pos_sh+3] << 24) | (puntero[pos_sh+2] << 16) | (puntero[pos_sh+1] << 8) | puntero[pos_sh] ) == 9)	// si sh_type es 9 (SHT REL)
-						{
-							pos_sh=pos_sh+12;	// Nos saltamos sh_flags y sh_addr
-							sh_offset = (puntero[pos_sh+3] << 24) | (puntero[pos_sh+2] << 16) | (puntero[pos_sh+1] << 8) | puntero[pos_sh];
-							pos_sh=pos_sh+4;
-							sh_size = 	(puntero[pos_sh+3] << 24) | (puntero[pos_sh+2] << 16) | (puntero[pos_sh+1] << 8) | puntero[pos_sh];
-							pos_sh=pos_sh+4;
-							sh_link = 	(puntero[pos_sh+3] << 24) | (puntero[pos_sh+2] << 16) | (puntero[pos_sh+1] << 8) | puntero[pos_sh];
-							pos_sh=pos_sh+4;
-							sh_info = 	(puntero[pos_sh+3] << 24) | (puntero[pos_sh+2] << 16) | (puntero[pos_sh+1] << 8) | puntero[pos_sh];
-							pos_sh=pos_sh+8;	// Nos saltamos sh_addralign
-							sh_entsize =(puntero[pos_sh+3] << 24) | (puntero[pos_sh+2] << 16) | (puntero[pos_sh+1] << 8) | puntero[pos_sh];
-							
-							//printf("%d %d %d %d %d\n", sh_offset, sh_size, sh_link, sh_info, sh_entsize);
-							
-							
-							pos_r = sh_offset;							// Trabajaremos con las relocalizaciones
-							pos_r_ini = pos_r;
-							printf(">> seccion relocalizadores ( ):\n");
-							
-							for (contador_r=0; contador_r<(sh_size/sh_entsize); contador_r++, pos_r=pos_r_ini+sh_entsize, pos_r_ini=pos_r)
-							{
-								// if r_info == 514 (0x0202)  -- r_info tiene un offset inicial de 4 bytes respecto a r_offset, de ahí el  4-7 en lugar del típico 0-3
-								if ( ( (puntero[pos_r+7] << 24) | (puntero[pos_r+6] << 16) | (puntero[pos_r+5] << 8) | puntero[pos_r+4]) == 514)
-								{
-									r_offset =	(puntero[pos_r+3] << 24) | (puntero[pos_r+2] << 16) | (puntero[pos_r+1] << 8) | puntero[pos_r];
-									printf(">>> rel %d (R_ARM_ABS32): 0x%X\n", contador_r, r_offset);
-									
-									
-									
-									destino = (unsigned int*)((int)0x02100000 + r_offset - p_paddr);
-									//_gm_relocatar((unsigned int*)r_offset, puntero, p_paddr);	// llamar función relocalizadora
-									
-									
-									//_gm_relocatar(destino, puntero, p_paddr);	// llamar función relocalizadora
-									_gm_relocatar(destino, puntero, p_paddr);	// llamar función relocalizadora
-									
-									
-									//puntero[(int)destino] = (int)0x02100000 + puntero[(int)destino] - p_paddr ;
-									
-								}	//endif r
-								
-							}	// endfor r
-							
-						}	// endif sh
-						
-					}	// endfor sh
+					
 					
 				}	// endif p
 				
@@ -214,7 +229,7 @@ intFunc _gm_cargarPrograma(char *keyName) {
 			
 			
 			free(puntero);
-			result = (int)0x02100000 + entry - p_paddr;									// La función debe devolver esta dirección de arranque
+			result = MEMORIA_PROGRAMA + entry - p_paddr;				// La función debe devolver esta dirección de arranque
 			
 		}
 		
